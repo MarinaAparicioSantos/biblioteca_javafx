a04a24edcd0997a8ba894be8b3118dfc7d04559e
Marina <marina.aparicio.santos.al@iespoligonosur.org>
2021-04-13 14:06:43 +0200

    13/04

--- src/main/java/gui/viewsandcontrollers/form/LibroFormController.java
+++ src/main/java/gui/viewsandcontrollers/form/LibroFormController.java
@@ -1,5 +1,55 @@
 package gui.viewsandcontrollers.form;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class LibroFormController {
+
+    @FXML
+    private TextField titulo;
+
+    @FXML
+    private TextField isbn;
+
+    @FXML
+    private TextField autor;
+
+    @FXML
+    private TextField paginas;
+
+    @FXML
+    private ChoiceBox<?> genero;
+
+    @FXML
+    private Button guardar;
+
+    @FXML
+    void añadirTitulo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void añadirautor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void añadirisbn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void añadirpaginas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/gui/viewsandcontrollers/main/MainStageController.java
+++ src/main/java/gui/viewsandcontrollers/main/MainStageController.java
@@ -1,5 +1,49 @@
 package gui.viewsandcontrollers.main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainStageController {
+
+    @FXML
+    private Button nuevo;
+
+    @FXML
+    private Button editar;
+
+    @FXML
+    private Button eliminar;
+
+    @FXML
+    private Button salvar;
+
+    @FXML
+    private Button cargar;
+
+    @FXML
+    void cargar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarlistalibros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarlibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void libronuevo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
 
 }
