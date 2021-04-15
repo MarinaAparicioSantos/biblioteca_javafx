1d7602af35c2e0c6bb7624ebb22487a0ba981804
Marina <marina.aparicio.santos.al@iespoligonosur.org>
2021-04-15 12:27:49 +0200

    15/04

--- src/main/java/gui/viewsandcontrollers/main/MainStageController.java
+++ src/main/java/gui/viewsandcontrollers/main/MainStageController.java
@@ -1,49 +1,83 @@
 package gui.viewsandcontrollers.main;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import negocio.model.Libro;
 
 public class MainStageController {
 
-    @FXML
-    private Button nuevo;
+	@FXML
+	private Button nuevo;
 
-    @FXML
-    private Button editar;
+	@FXML
+	private Button editar;
 
-    @FXML
-    private Button eliminar;
+	@FXML
+	private Button eliminar;
 
-    @FXML
-    private Button salvar;
+	@FXML
+	private Button salvar;
 
-    @FXML
-    private Button cargar;
+	@FXML
+	private Button cargar;
 
-    @FXML
-    void cargar(ActionEvent event) {
+	@FXML
+	private TableView tabla;
 
-    }
+	public void initialize() {
+	
+		  List<Libro> table = new ArrayList<Libro>();
+		  ObservableList<Libro> observableList = FXCollections.observableList(table);
+	}
 
-    @FXML
-    void editarlistalibros(ActionEvent event) {
+	@FXML
+	void cargar(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void eliminarlibro(ActionEvent event) {
+	@FXML
+	void editarlistalibros(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void libronuevo(ActionEvent event) {
+	@FXML
+	void eliminarlibro(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void salvar(ActionEvent event) {
+	@FXML
+	void libronuevo(ActionEvent event) throws IOException {
 
-    }
+		Stage dialog = new Stage();
+		Node source = (Node) event.getSource();
+		Stage parent = (Stage) source.getScene().getWindow();
+
+		dialog.initOwner(parent);
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		Parent root = FXMLLoader.load(getClass().getResource("../form/LibroForm.fxml"));
+		Scene scene = new Scene(root);
+		dialog.setScene(scene);
+		dialog.show();
+
+	}
+
+	@FXML
+	void salvar(ActionEvent event) {
+
+	}
 
 }
--- src/main/java/negocio/model/Libro.java
+++ src/main/java/negocio/model/Libro.java
@@ -1,5 +1,107 @@
 package negocio.model;
 
-public class Libro {
+public class Libro{
 
+	private String titulo;
+	private String isbn;
+	private Genero genero;
+	private String autor;
+	private Integer paginas;
+
+	public Libro() {
+
+	}
+
+	public Libro(String titulo, String isbn, Genero genero, String autor, Integer paginas) {
+		super();
+		this.titulo = titulo;
+		this.isbn = isbn;
+		this.genero = genero;
+		this.autor = autor;
+		this.paginas = paginas;
+	}
+
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	/**
+	 * @return the isbn
+	 */
+	public String getIsbn() {
+		return isbn;
+	}
+
+	/**
+	 * @param isbn the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	/**
+	 * @return the genero
+	 */
+	public Genero getGenero() {
+		return genero;
+	}
+
+	/**
+	 * @param genero the genero to set
+	 */
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	/**
+	 * @return the autor
+	 */
+	public String getAutor() {
+		return autor;
+	}
+
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	/**
+	 * @return the paginas
+	 */
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	/**
+	 * @param paginas the paginas to set
+	 */
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+
+	@Override
+	/**Metodo toString de la clase Libro que muestra los datos de los libros.
+	 * @return retorno
+	 */
+	public String toString() {
+		String retorno;
+		retorno = "Titulo: " + titulo + "\n";
+		retorno = retorno + "isbn: " + isbn + "\n";
+		retorno = retorno + "Genero: " + genero + "\n";
+		retorno = retorno + "Autor: " + autor + "\n";
+		retorno = retorno + "Paginas: " + paginas + "\n";
+		return retorno;
+	}
 }
